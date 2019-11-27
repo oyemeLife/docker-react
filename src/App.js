8f96885b746c6a41ed26c7702944a3e790796077
Andrejs Aleksejevs <andrejs.aleksejevs@vuelio.com>
2019-11-27 18:38:26 +0000

    app text updated

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. AA
-          SEX
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>“Muddy water is best cleared by leaving it alone.” – Alan Watts,</p>
+                <p> Just a test page :)</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
